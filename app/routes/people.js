1d5f125657d75b41dff776d1bc50fedb05e8a814
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2020-02-13 15:42:13 +0300

    feat: edit card component

--- app/routes/people.js
+++ app/routes/people.js
@@ -1,20 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  // async model() {
-  //   const url = 'https://swapi.co/api/people/';
-  //   let response = await fetch(url);
-  //   let { count } = await response.json();
-  //   let random = () => {
-  //     const min = 1;
-  //     const max = count;
-  //     return Math.floor(Math.random() * (max - min)) + min;
-  //   };
-  //   let cardResponsePlayer = await fetch(`${url}${random()}/`);
-  //   let resultPlayer = await cardResponsePlayer.json();
-  //
-  //   let cardResponseComputer = await fetch(`${url}${random()}/`);
-  //   let resultComputer = await cardResponseComputer.json();
-  //   return {resultPlayer, resultComputer};
-  // }
+
 }
--- app/routes/starships.js
+++ app/routes/starships.js
@@ -1,9 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class StarshipsRoute extends Route {
-  async model() {
-    let response = await fetch('https://swapi.co/api/starships/');
-    let { results } = await response.json();
-    return results;
-  }
+
 }
