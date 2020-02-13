834bef43c27e5206ae9ebb829176c69805332e10
Alena Kharytonchyk <alena.kharytonchyk@outlook.com>
2020-02-13 23:12:33 +0300

    feat: add deployment scripts

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    ENV.rootURL = '/Ember_Star-Wars';
+    ENV.locationType = 'hash';
   }
 
   return ENV;
